ef28a0908cdc452fa616edaf5de9ae4f35e3faab
icosac <enricosaccon96@gmail.com>
2024-06-29 20:09:35 +0200

    fixes plus kitty

--- ubuntu/22/install.sh
+++ ubuntu/22/install.sh
@@ -12,6 +12,7 @@ fi
 
 #List of all possible packets to install
 LIST=(\
+  cuda\
   neovim\
   vim\
   tmux\
@@ -20,7 +21,8 @@ LIST=(\
   docker\
   latex\
   regolith\
-  code
+  code\
+  kitty
 ) 
 
 # The script will probably be executed with sudo. 
@@ -70,15 +72,81 @@ INFO $BGREEN "About to install packages: ${packages[*]}"
 
 # Update system before starting
 INFO $BBLUE "Updating system"
-# $UPDATE &> /dev/null
+$UPDATE &> /dev/null
 
 # Install packages
 
+# KITTY
+if in_list "${packages[*]}" "kitty"; then
+  INFO ${BBLUE} "KITTY"
+  INFO ${GREEN} "Installing Kitty"
+  $IN kitty &> /dev/null
+  INFO ${GREEN} "Installing JetBrains fonts"
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip -O font.zip &> /dev/null
+  unzip -f font.zip -d ${real_home}/.local/share/fonts &> /dev/null
+  rm font.zip
+  sudo -u ${real_user} fc-cache -f -v &> /dev/null
+  INFO ${GREEN} "Configuring" 
+  mkdir -p ${real_home}/.config/kitty
+  cp -r ./kitty/* ${real_home}/.config/kitty/
+  sudo chown -R ${real_user}:${real_user} ${real_home}/.config/kitty
+fi
+
+# OH MY ZSH
+if in_list "${packages[*]}" "zsh"; then
+  INFO ${BBLUE} "OH MY ZSH"
+  INFO ${GREEN} "Installing dependencies"
+  dep "${IN}" "apt-req/zsh.txt"                                                                       &> /dev/null
+  if [ $? -eq 0 ]; then
+    INFO ${GREEN} "Installing zsh package"
+    $IN zsh &> /dev/null                                                                                         &> /dev/null
+    INFO ${GREEN} "Installing oh-my-zsh"
+    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O zsh_install.sh  &> /dev/null
+    sudo -u ${real_user} sed -i "s/RUNZSH=no; CHSH=no/RUNZSH=no/g" zsh_install.sh                     1> /dev/null
+    sudo -u ${real_user} yes | sudo -u ${real_user} sh -c "$(cat zsh_install.sh)" "" --unattended     &> /dev/null
+    sudo -u ${real_user} sed -i "s/robbyrussell/bira/g" ${real_home}/.zshrc                           1> /dev/null
+    sudo -u ${real_user} chsh -s /usr/bin/zsh
+    rm zsh_install.sh
+  else
+    ERROR "Could not install dependencies"
+  fi
+fi
+
+
 # CODE
-if in_list "${packages[*]}" "latex"; then
+if in_list "${packages[*]}" "code"; then
   wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/5437499feb04f7a586f677b155b039bc2b3669eb/code_1.90.2-1718751586_amd64.deb -O code.deb
   sudo dpkg -i code.deb
   rm code.deb
+fi
+
+
+# CUDA
+if in_list "${packages[*]}" "cuda"; then
+	INFO ${BBLUE} "Installing CUDA"
+	INFO ${GREEN} "Checking GPU support"
+	if [[ $(lshw -C display | grep vendor) =~ NVIDIA ]]; then
+		INFO ${GREEN} "Installing dependencies"
+		dep ${IN} "apt-req/cuda.txt"                                                                        &> /dev/null
+		wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin &> /dev/null
+		sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+		INFO ${GREEN} "Downloading CUDA from Nvidia"
+		wget https://developer.download.nvidia.com/compute/cuda/12.5.0/local_installers/cuda-repo-ubuntu2204-12-5-local_12.5.0-555.42.02-1_amd64.deb -O cuda.deb &> /dev/null
+		sudo dpkg -i cuda.deb                                                                               &> /dev/null
+		sudo apt -f install                                                                                 &> /dev/null
+		sudo cp /var/cuda-repo-ubuntu2204-12-5-local/cuda-*-keyring.gpg /usr/share/keyrings/
+		$UPDATE                                                                                             &> /dev/null
+		INFO ${GREEN} "Installing CUDA and nvcc"
+		${IN} cuda-toolkit-12-5 cuda-nvcc-12-5                                                              &> /dev/null
+		sudo rm cuda.deb 
+		if [[ "${real_shell}" == *"zsh" ]]; then 
+			echo -e "PATH=$PATH:/usr/local/cuda-12.5/bin/" >> "${real_home}/.zshrc"
+    elif [[ "${real_shell}" == *"bash" ]]; then 
+			echo -e "PATH=$PATH:/usr/local/cuda-12.5/bin/" >> "${real_home}/.bashrc"
+    fi
+	else
+		ERROR "No Nvidia GPU available"
+	fi
 fi
 
 
@@ -86,15 +154,15 @@ fi
 if in_list "${packages[*]}" "latex"; then
   INFO ${BBLUE} "LATEX"
   INFO ${GREEN} "Installing apt dependencies"
-  dep "$IN" "apt-req/latex.txt" &> /dev/null
+  dep "$IN" "apt-req/latex.txt"                                                                                       &> /dev/null
   INFO ${GREEN} "Installing python3 dependencies"
-  sudo -u ${real_user} python3 -m pip install -U -r pip-req/latex.txt &> /dev/null
+  sudo -u ${real_user} python3 -m pip install -U -r pip-req/latex.txt                                                 &> /dev/null
   INFO ${GREEN} "Installing main packages"
   $IN texlive-base texlive-plain-generic texlive-latex-recommended texlive-latex-extra texlive-science texlive-luatex &> /dev/null 
   INFO ${GREEN} "Configuring"
   $MOVE latexmkrc ${real_home}/.latexmkrc
 
-  command -v pygmentize &> /dev/null
+  command -v pygmentize                                                                                               &> /dev/null
   if [ $? -ne 0 ]; then
     if [[ "${real_shell}" == *"zsh" ]]; then 
       echo -e "# Python3 PIP packages\nexport PATH=\"\${PATH}:${real_home}/.local/bin\"" >> "${real_home}/.zshrc"
@@ -108,11 +176,11 @@ fi
 if in_list "${packages[*]}" "docker"; then
   INFO ${BBLUE} "DOCKER"
   INFO ${GREEN} "Installing dependencies"
-  dep "${IN}" "apt-req/docker.txt" &> /dev/null
+  dep "${IN}" "apt-req/docker.txt"                                &> /dev/null
   INFO ${GREEN} "Adding keys and repos"
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  $UPDATE &> /dev/null
+  $UPDATE                                                         &> /dev/null
   INFO ${GREEN} "Installing docker packages"
   $IN docker-ce docker-ce-cli containerd.io docker-compose-plugin &> /dev/null
   INFO ${GREEN} "Running Docker post-installation steps"
@@ -127,38 +195,44 @@ fi
 
 # GIT
 if in_list "${packages[*]}" "git"; then  
-  $INFO ${BBLUE} "GIT"
-  $IN git
+  INFO ${BBLUE} "GIT"
+  INFO ${GREEN} "Installing git"
+  $IN git ^> /dev/null
+  INFO ${GREEN} "Reading and setting the configuration from ${}/git/config"
+  . git/config
+  sudo -u ${real_user} git config --global user.email $git_email
+  sudo -u ${real_user} git config --global user.name $git_username
+
   read -t 5 -p "If you want to take control and generate passkeys, write y in 5 seconds [y/N] " choice
   if [ $? -eq 0 ] && { [ "${choice}" = "y" ] || [ "${choice}" = "Y" ]; }; then
-    INFO ${GREEN} "ssh dir in ${SSH_DIR}"
+    INFO ${GREEN} "Installing additional dependencies"
+    dep ${IN} "apt-req/git.txt"
     SSH_DIR=${real_home}/.ssh
     if [ ! -d $SSH_DIR ]; then
       mkdir -p $SSH_DIR
       chown ${real_user}:${real_user} $SSH_DIR 
       chmod 700 $SSH_DIR
     fi
-    INFO ${GREEN} "Installing additional dependencies"
-    # dep "${IN} "apt-req/git.txt"
+    INFO ${GREEN} "ssh dir at ${SSH_DIR}"
+    
     INFO ${GREEN} "Asking for passkeys"
-
     GIT_HOSTS=(github gitlab bitbucket)
     for GIT_HOST in "${GIT_HOSTS[@]}"; do
       choice="no"
       read -p "Do you want to create a passkey for $GIT_HOST? [y/N] " choice
       if [ "${choice}" = "y" ] || [ "${choice}" = "Y" ]; then
-	# Create key
+	      # Create key
         ssh-keygen -t rsa  -f $SSH_DIR/$GIT_HOST
-	# Change comment from root@$HOSTNAME
-	ssh-keygen -c -C "${real_user}@$HOSTNAME" -f $SSH_DIR/$GIT_HOST
+        # Change comment from root@$HOSTNAME
+        ssh-keygen -c -C "${real_user}@$HOSTNAME" -f $SSH_DIR/$GIT_HOST
         chown $real_user:$real_user $SSH_DIR/$GIT_HOST*
         chmod 644 $SSH_DIR/$GIT_HOST.pub
         chmod 600 $SSH_DIR/$GIT_HOST
-	echo -e "\nHost ${GIT_HOST}_dotfile\n  HostName ${GIT_HOST}.com\n  IdentityFile ${SSH_DIR}/${GIT_HOST}\n  IdentitiesOnly yes\n" >> $SSH_DIR/config
-	sed -i "s/ root@/ ${real_user}@/g" $SSH_DIR/config
-	if [ "$GIT_HOST" == "bitbucket" ]; then
-	  sed -i "s/bitbucket.com/bitbucket.org/g" $SSH_DIR/config
-	fi
+        echo -e "\nHost ${GIT_HOST}_dotfile\n  HostName ${GIT_HOST}.com\n  IdentityFile ${SSH_DIR}/${GIT_HOST}\n  IdentitiesOnly yes\n" >> $SSH_DIR/config
+        sed -i "s/ root@/ ${real_user}@/g" $SSH_DIR/config
+        if [ "$GIT_HOST" == "bitbucket" ]; then
+          sed -i "s/bitbucket.com/bitbucket.org/g" $SSH_DIR/config
+        fi
       fi
     done
     INFO ${GREEN} "The script will now continue without interruptions"
@@ -168,29 +242,10 @@ if in_list "${packages[*]}" "git"; then
   INFO ${BLUE} "Git installed"
 fi
 
-# OH MY ZSH
-if in_list "${packages[*]}" "zsh"; then
-  INFO ${BBLUE} "OH MY ZSH"
-  INFO ${GREEN} "Installing dependencies"
-  dep "${IN}" "apt-req/zsh.txt" &> /dev/null
-  if [ $? -eq 0 ]; then
-    INFO ${GREEN} "Installing zsh package"
-    $IN zsh &> /dev/null
-    INFO ${GREEN} "Installing oh-my-zsh"
-    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O zsh_install.sh  &> /dev/null
-    sudo -u ${real_user} sed -i "s/RUNZSH=no; CHSH=no/RUNZSH=no/g" zsh_install.sh                     1> /dev/null
-    sudo -u ${real_user} sh -c "$(cat zsh_install.sh)" "" --unattended                                &> /dev/null
-    sudo -u ${real_user} sed -i "s/robbyrussell/bira/g" ${real_home}/.zshrc                           1> /dev/null
-    rm zsh_install.sh
-  else
-    ERROR "Could not install dependencies"
-  fi
-fi
-
 # REGOLITH
-if in_list "${packages[*]}" "zsh"; then
+if in_list "${packages[*]}" "regolith"; then
   wget -qO - https://regolith-desktop.org/regolith.key | \
-  gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+  gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg &> /dev/null
   echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] https://regolith-desktop.org/release-3_1-ubuntu-jammy-amd64 jammy main" | \
   sudo tee /etc/apt/sources.list.d/regolith.list
   $UPDATE &> /dev/null
@@ -213,13 +268,17 @@ if in_list "${packages[*]}" "neovim"; then
   elif [[ "${real_shell}" == *"bash" ]]; then 
     echo "export PATH=\"$PATH:/opt/nvim-linux64/bin\"" >> "${real_home}/.bashrc"
   fi
-  INFO ${BGREEN} "neovim installed, resource your shell"
+  INFO ${BGREEN} "neovim installed, re-source your shell"
 
   INFO ${GREEN} "Installing Astronvim"
   rm -rf ${real_home}/.config/nvim
   rm -rf ${real_home}/.local/share/nvim
   rm -rf ${real_home}/.local/state/nvim
   rm -rf ${real_home}/.cache/nvim
+  #############################################################################
+  # TODO REMOVE THIS LINE
+  #############################################################################
+  if [ -d "${real_home}/.config/nvim-profiles" ]; then rm -Rf $WORKING_DIR; fi
   sudo -u ${real_user} mkdir -p ${real_home}/.config/nvim-profiles/
   sudo -u ${real_user} git clone --depth 1 https://github.com/AstroNvim/template ${real_home}/.config/nvim-profiles/astronvim &> /dev/null
   sudo -u ${real_user} rm -rf ${real_home}/.config/nvim-profiles/astronvim/.git
@@ -230,11 +289,11 @@ if in_list "${packages[*]}" "neovim"; then
   if [ ! -d ${real_home}/.local/share/fonts ]; then
     mkdir ${real_home}/.local/share/fonts
   fi
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/VictorMono.zip
-  # wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip &> /dev/null
-  unzip -f VictorMono.zip -d ${real_home}/.local/share/fonts
-  rm VictorMono.zip
-  fc-cache -f -v
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/VictorMono.zip -O font.zip &> /dev/null
+  # wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip -O font.zip &> /dev/null
+  unzip -f font.zip -d ${real_home}/.local/share/fonts &> /dev/null
+  rm font.zip
+  sudo -u ${real_user} fc-cache -f -v &> /dev/null
 
   INFO ${GREEN} "Configuring Astronvim"
   sudo -u "${real_user}" bash -c 'NVIM_APPNAME=nvim-profiles/astronvim /opt/nvim-linux64/bin/nvim +qall'
